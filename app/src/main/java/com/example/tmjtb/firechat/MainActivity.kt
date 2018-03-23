d8a24b41b633c11c6beccdec611285f5703007e5
johtmatt <31523483+johtmatt@users.noreply.github.com>
2018-03-23 11:23:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tmjtb/firechat/MainActivity.kt
@@ -0,0 +1,178 @@
+package com.example.tmjtb.firechat
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private var mAuth: FirebaseAuth? = null
+    private var mUser: FirebaseUser? = null
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = mAuth?.getCurrentUser()
+        updateUI(currentUser)
+    }
+
+    public fun updateUI(currentUser: FirebaseUser?) {
+        mUser = currentUser
+
+        if (currentUser != null) {
+            buttonLogout.visibility = View.VISIBLE
+            buttonLogin.visibility = View.GONE
+            textUserName.text = currentUser?.displayName
+            textUserEmail.text = currentUser?.email
+            textUserID.text = currentUser?.uid
+
+            Picasso.with(this@MainActivity)
+                    .load(currentUser?.photoUrl)
+                    .into(imageProfile)
+        }
+
+        else {
+            buttonLogout.visibility = View.GONE
+            buttonLogin.visibility = View.VISIBLE
+            textUserName.text = "NOT LOGGED IN"
+            textUserEmail.text = ""
+            textUserID.text = ""
+            imageProfile.setImageResource(R.drawable.common_google_signin_btn_text_disabled)
+        }
+
+        // todo
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        buttonLogin.setOnClickListener {view -> doLoginStuff() }
+        buttonLogout.setOnClickListener {view -> doLogoutStuff() }
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    val RC_SIGN_IN = 123
+
+    fun doLoginStuff() {
+        val providers = Arrays.asList(
+                AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
+                AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()
+        )
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN)
+
+    }
+
+    fun doLogoutStuff() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener {
+                    this.updateUI(null)
+                }
+    }
+
+    override fun onAcitivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+                this.updateUI(user)
+            }
+
+            else {
+                this.updateUI(null)
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
